b77a2b8e8b532f7302bc8427c0946c20a09b78a7
fastmans2 <milanmarkovic999999999@gmail.com>
2020-03-24 16:04:33 +0100

    e

--- p/kalendar/dates.js
+++ p/kalendar/dates.js
@@ -5,6 +5,7 @@ calendarData.push('{"predmet":"Engleski","month":"3","day":"25","clickUrl":"http
 calendarData.push('{"predmet":"Mazicka","month":"3","day":"27","clickUrl":"https://classroom.google.com/u/1/c/NTQ2NTk0MzEyNzVa"}');
 calendarData.push('{"predmet":"Mreze","month":"3","day":"27","clickUrl":"https://classroom.google.com/u/1/c/NTM4NDAyMTQ1NTla/a/NTU0ODQ3NTQwOTFa/details"}');
 calendarData.push('{"predmet":"Operativni","month":"3","day":"30","clickUrl":"https://classroom.google.com/u/1/c/NTQ1Mzk4MDMwOTla/m/NTU0NDAzMDQyMjNa/details"}');
+calendarData.push('{"predmet":"Matis","month":"3","day":"30","clickUrl":"https://classroom.google.com/u/1/c/NjEyMjUxMjc3OTla/a/NTUzMjg2MTkwODBa/details"}');
 
 var unknownData = new Array();
 unknownData.push('{"predmet":"Ivona","clickUrl":"https://drive.google.com/file/d/1TSIhS_wVBtBFdxE4J1_oVOsGfkcuq6er/view"}');
