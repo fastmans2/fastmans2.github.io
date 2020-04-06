5bf4244b5f4eb0f8507e5ce81201ebaa0e30d6ee
fastmans2 <milanmarkovic999999999@gmail.com>
2020-04-06 22:12:36 +0200

    asdada

--- p/kalendar/data.js
+++ p/kalendar/data.js
@@ -17,6 +17,7 @@ calendarData.push('{"predmet":"Mreze","month":"4","day":"14","clickUrl":"https:/
 calendarData.push('{"predmet":"Rakicka","month":"4","day":"3","clickUrl":"https://classroom.google.com/u/1/c/NTM2MzE3ODIyMzda/a/NzQ1NDQwMzY5NDFa/details"}');
 calendarData.push('{"predmet":"Srpski","month":"4","day":"8","clickUrl":"https://classroom.google.com/u/1/c/NTQ2Mjc5NjY5NDla/a/NzUwMDUzMzg3Mjda/details","time":"12:00"}');
 calendarData.push('{"predmet":"Program","month":"4","day":"7","clickUrl":"https://classroom.google.com/u/1/c/NTM4NDAyMTQ1MTFa/a/NzQ4MDg5NDg1Mjda/details","time":"08:00"}');
+calendarData.push('{"predmet":"Rakicka","month":"4","day":"6","clickUrl":"https://classroom.google.com/u/1/c/NTQ1Mzk4MDMwOTla/a/NzQ3ODgyMDA3NTBa/details"}');
 
 var unknownData = new Array();
 unknownData.push('{"predmet":"Ivona","clickUrl":"https://drive.google.com/file/d/1TSIhS_wVBtBFdxE4J1_oVOsGfkcuq6er/view"}');
