c2d34dc2ef392423be203b6740ee9a6b0c84e390
Hominglesi <milanmarkovic999999999@gmail.com>
2022-01-14 03:35:05 +0100

    Moved site

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('mousedown', function (event) {
-  if (event.detail > 1) {
-    event.preventDefault();
-  }
-}, false);
-
-//da ovaj fajl samo postoji zbog ove jedne funkcije koji ja nisam ni napisao 
